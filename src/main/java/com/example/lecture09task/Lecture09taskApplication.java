41309eb80117f261b8c36f0b298800d7e0904830
UserName <rin3810400h@gmail.com>
2023-05-02 16:58:40 +0900

    第9回課題提出

---
+++ src/main/java/com/example/lecture09task/Lecture09taskApplication.java
@@ -0,0 +1,13 @@
+package com.example.lecture09task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture09taskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture09taskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lecture09task/Lecture09taskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecture09task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture09taskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
