08d009c33171e100f2bdd2224a19963c769e08e7
UserName <rin3810400h@gmail.com>
2023-05-17 23:37:24 +0900

    UserMaperのimportを修正

--- src/main/java/com/example/lecture09task/mapper/UserMapper.java
+++ src/main/java/com/example/lecture09task/mapper/UserMapper.java
@@ -1,7 +1,11 @@
 package com.example.lecture09task.mapper;
 
 import com.example.lecture09task.entity.User;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
