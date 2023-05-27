dfcc6e827b6340db920e3cd9b9d7674d9839be92
UserName <rin3810400h@gmail.com>
2023-05-27 20:04:38 +0900

    ServiceImplの単体テストを実装（再コミット）

--- src/main/java/com/example/lecture09task/form/CreateForm.java
+++ src/main/java/com/example/lecture09task/form/CreateForm.java
@@ -2,10 +2,11 @@ package com.example.lecture09task.form;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-
+@AllArgsConstructor
 public class CreateForm {
 
     @NotBlank
